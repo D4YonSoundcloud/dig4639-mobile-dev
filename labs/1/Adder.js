2a32337175bfa343ad101f7501ac8272d53c37eb
D4YonSoundcloud <mattd4y@gmail.com>
2020-01-28 14:23:02 -0500

    done 2 22pm 1/28

--- labs/1/Adder.js
+++ labs/1/Adder.js
@@ -2,19 +2,19 @@ class Adder {
   constructor(a, b) {
     this.a = a;
     this.b = b;
-  }
-
-  add() {
-    return this.a+this.b;
+    this.sum = function() {
+      let z = a + b;
+      if (a && b) {
+        return `${z}`;
+      } else {
+        return `null`;
+      }
+    };
   }
 
   render() {
-    return `<p> The sum of ${this.a} and ${this.b} is </p>`;
+    return `<p>The sum of ${this.a} and ${this.b} is ${this.sum()}</p>`;
   }
 }
 
 module.exports = Adder;
-
-Adder.sum = function(){
-    return this.a + this.b;
-}
