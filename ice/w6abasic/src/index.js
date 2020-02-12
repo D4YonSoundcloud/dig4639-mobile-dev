68f6fe0cedd3f5f9e574fb5f921d9d3fd0613484
Matthew Day <ma921903@ucf.edu>
2020-02-12 11:25:00 -0500

    212 es lint

---
+++ ice/w6abasic/src/index.js
@@ -0,0 +1,69 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const Square = props => {
+  return <button className="square">{this.props.value}</button>;
+};
+
+class Board extends React.Component {
+  state = {
+    squares: Array(9).fill("X"),
+    xIsNext: true
+  };
+
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} onClick={onClick} />;
+  }
+
+  onClick() {
+    xIsNext = this.state.xIsNext;
+    nextMove = this.setState({ xIsNext: !xIsNext });
+    nextMove;
+  }
+
+  render() {
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
