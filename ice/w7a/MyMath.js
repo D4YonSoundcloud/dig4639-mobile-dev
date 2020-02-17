a1c094e64d8413acc80b35111cdc83cae4fc95a4
Matthew Day <ma921903@ucf.edu>
2020-02-17 12:23:37 -0500

    intro to jest

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,3 +1,69 @@
-export function Sum (a, b) {
-  return a + b
+export function Sum(a, b) {
+  let result = undefined;
+  if (typeof a === "number" && typeof b === "number") {
+    result = a + b;
+  } else {
+    return undefined;
+  }
+  return result;
 }
+
+//console.log(Sum(2,3)==5)
+let myArray = [];
+
+export function AddList(myArray) {
+  let result = undefined;
+  if(Array.isArray(myArray) && myArray.length > 0){
+    result = 0;
+  for(var i = 0; i < myArray.length; i++){
+    if(typeof myArray[i] != "number"){
+      return undefined
+    }
+        result = result + myArray[i];
+  }
+}
+  return result;
+  // if (Array.isArray(newArray) && !newArray.some(isNaN)) {
+  //   let addedlist = newArray.reduce((total, num) => total + num);
+  //   if (addedList === 0) {
+  //     return null;
+  //   } else {
+  //     return addedlist;
+  //   }
+  // }
+  // return undefined;
+}
+
+export function DivideBy(number, divisor) {
+  if (divisor != 0 && isNaN(divisor)) {
+    var sum = number % divisor;
+    return sum;
+  } else {
+    return undefined;
+  }
+}
+
+export function ContainsString(string1, string2) {
+  if (
+    typeof string1 === "string" &&
+    typeof string2 === "string" &&
+    string1.includes(string2)
+  ) {
+    return true;
+  } else {
+    return "enter a string";
+  }
+}
+
+export function ReSortedNumbers([]) {
+  let newArray = [...[]];
+  let sortedArray = newArray.sort();
+  if(sortedArray.filter(isNaN)){
+    return sortedArray;
+  } else {
+    return null;
+  }
+
+}
+
+export function Adder() {}
--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,27 +1,69 @@
-import { Sum } from './sum.js'
+import { Sum } from "./MyMath.js";
+import { AddList } from "./MyMath.js";
+import { DivideBy } from "./MyMath.js";
+import { ContainsString } from "./MyMath.js";
+import { ReSortedNumbers } from "./MyMath.js";
+import { Adder } from "./MyMath.js";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(Sum(1, 2)).toBe(3)
-})
+describe("Sum", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(Sum(1, 2)).toBe(3);
+  });
+  it("whether undefined is returned on invalid type", () => {
+    expect(Sum(1, "Test")).toBeUndefined();
+  });
+});
 
-// ICE 
+describe("AddList", () => {
+  let numElem = 1000;
+  let input = [];
+  let result = 0;
+  let element = 0
+  for(var j = 0; j < 10; j++) {
+    for(var i = 0; i < numElem; i++) {
+      elem = Math.random()*1000;
+      result += elem;
+      input.push(elem)
+    }
+    expect(Addlist(input)).toBe(result)
+  }
+  test("adds numbers in array", () => {
+    expect(AddList(input)).toBe(result);
+  });
+  input = [15,3,4,5];
+  result = [27];
+  test("adds numbers in array", () => {
+    let input = [];
+    expect(AddList(input)).toBeUndefined();
+  });
+  test("adds numbers in array", () => {
+    let input = [undefined,0,2,3];
+    expect(AddList(input)).toBeUndefined();
+  });
+  test("adds numbers in array", () => {
+    let input = 100;
+    expect(AddList(input)).toBeUndefined();
+  });
+});
+
+// ICE
 
 // Examples for Null
-test('null', () => {
-  const n = null
-  expect(n).toBeNull()
-  expect(n).toBeDefined()
-  expect(n).not.toBeUndefined()
-  expect(n).not.toBeTruthy()
-  expect(n).toBeFalsy()
-})
+test("null", () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
 
 // Examples for zero
-test('zero', () => {
-  const z = 0
-  expect(z).not.toBeNull()
-  expect(z).toBeDefined()
-  expect(z).not.toBeUndefined()
-  expect(z).not.toBeTruthy()
-  expect(z).toBeFalsy()
-})
+test("zero", () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
