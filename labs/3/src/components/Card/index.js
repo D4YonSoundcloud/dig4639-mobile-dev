f724df71b55841b83c435f28eece44c13e9f6462
d4yonsouncloude <mattd4y@gmail.com>
2020-02-23 21:49:21 -0600

    9:47 lab 3 finished

---
+++ labs/3/src/components/Card/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './index.css';
+
+export class Card extends Component {
+    render() {
+        return (
+            <div className="card">
+                This is a card
+            </div>
+        )
+    }
+}
+
+export default Card
